867b3205cd06c476890c8cc30ea94a0c497e0a07
Olivier <2076572@cegepmontpetit.ca>
2025-09-22 13:40:30 -0400

    Étape 6.2 - Fonctionnalité et tests
    Modification du code pour la fonctionnalité et ajustement des tests.

--- test/core/jeuDeDes.test.ts
+++ test/core/jeuDeDes.test.ts
@@ -11,24 +11,25 @@ describe('JeuDeDesTest', () => {
     expect(jdd.joueurs).toEqual("[]")
   })
 
-  it('devrait retourner une valeur entre 2 et 12', () => {
+  it('devrait retourner une valeur entre 3 et 18', () => {
     for (let i = 0; i < 200; i++) {
-      expect(jdd.brasser()).toBeWithin(2, 13);
+      expect(jdd.brasser()).toBeWithin(3, 19);
     }
   })
 
-  it('devrait retourner finalement toutes les valeurs entre 2 et 12', () => {
+  it('devrait retourner finalement toutes les valeurs entre 3 et 18', () => {
     const resultats = new Set();
-    for (let i = 0; i < 200; i++) {
+    // Brasse les dés jusqu'à 2000 fois mais sors de la boucle une fois que tous les résultats possibles sont obtenus.
+    for (let i = 0; i < 2000 && resultats.size <16; i++) {
       resultats.add(jdd.brasser())
     }
-    expect(resultats.size).toBe(11);
-    for (let i = 1; i < 12; i++) {
+    expect(resultats.size).toBe(16);
+    for (let i = 3; i < 17; i++) {
       expect(resultats.has(i + 1)).toBeTrue();
     }
     // cas particuliers
     expect(resultats.has(1)).toBeFalsy();
-    expect(resultats.has(13)).toBeFalsy();
+    expect(resultats.has(2)).toBeFalsy();
+    expect(resultats.has(19)).toBeFalsy();
   })
-
 });
